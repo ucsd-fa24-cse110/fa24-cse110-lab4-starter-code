8980e7f067a15f43e2122a68e3ae903d717c1817
Natasha <natashamarietran@gmail.com>
2024-10-21 14:44:24 -0700

    EXERCISE 1 ALL DONE

--- src/components/Expense/ExpenseItem.tsx
+++ src/components/Expense/ExpenseItem.tsx
@@ -1,10 +1,17 @@
 import { Expense } from "../../types/types";
+import React, { useContext } from "react";
+import { AppContext } from "../../context/AppContext";
 
 const ExpenseItem = (currentExpense: Expense) => {
-  // Exercise: Consume the AppContext here
+  // Exercise: Consume the AppContext here DONE
+  const { expenses, setExpenses } = useContext(AppContext);
 
   const handleDeleteExpense = (currentExpense: Expense) => {
     // Exercise: Remove expense from expenses context array
+    const updated = expenses.filter(item => item.id !== currentExpense.id)
+    setExpenses(updated);
+    console.log(expenses)
+    
   };
 
   return (
