b5c5664f66209b830cfba08ad85fce51de1d9846
Natasha <natashamarietran@gmail.com>
2024-10-21 15:35:22 -0700

    midway thru the tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
@@ -7,3 +7,27 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+//OUR TESTS
+describe("Create an expense", () => {
+  test("renders create expense", () => {
+    render(<App />);
+
+    const createNoteNameInput = screen.getByLabelText("Name");
+    const createNoteCostInput = screen.getByLabelText("Cost");
+    const createNoteButton = screen.getByLabelText("Save");
+
+    fireEvent.change(createNoteNameInput, { target: {value: "New Expense" } });
+    fireEvent.change(createNoteCostInput, { target: {value: "New Cost" } });
+    fireEvent.click(createNoteButton);
+
+    const newNoteTitle = screen.getByText("New Expense");
+    const newNoteCost = screen.getByText("New Cost");
+
+    expect(newNoteTitle).toBeInTheDocument();
+    expect(newNoteCost).toBeInTheDocument();
+  });
+});
+
+
