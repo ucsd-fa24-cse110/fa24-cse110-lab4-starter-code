0288b85801d29c0ccfe77d095547b30771df154b
Yashwanth Ravipati <yashravipatischool@gmail.com>
2024-10-21 16:29:11 -0700

    Test 1

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("Create Expense", () => {
+  test("Renders AddExpenseForm", () => {
+      render(<App />); 
+
+      const nameInput = screen.getByLabelText("Name");
+      const costInput = screen.getByLabelText("Cost");
+      const saveButton = screen.getByText("Save");
+      expect(nameInput).toBeInTheDocument();
+      expect(costInput).toBeInTheDocument();
+      expect(saveButton).toBeInTheDocument();
+  });
+  
+  test("Creates new expense", () => {
+      render(<App />);
+
+      const nameInput = screen.getByLabelText("Name");
+      const costInput = screen.getByLabelText("Cost");
+      const saveButton = screen.getByText("Save");
+      fireEvent.change(nameInput, { target: { value: "New Expense" } });
+      fireEvent.change(costInput, { target: { value: "10" } });
+      fireEvent.click(saveButton);
+
+      const newExpense = screen.getByText("New Expense");
+      const newCost = screen.getByText("$10");
+      expect(newCost).toBeInTheDocument();
+      expect(newExpense).toBeInTheDocument();
+  })
+})
