a7f5a45be81ffa8eea199200e7112d166d175402
dhaDang <130107069+dhaDang@users.noreply.github.com>
2024-10-21 16:29:00 -0700

    update tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import { AppProvider } from './context/AppContext';
+import { render, screen, act } from '@testing-library/react';
+
+
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<AppProvider><App/></AppProvider>);
+  const linkElement = screen.getByText(/My Budget Planner/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/tests/BudgetTracker.test.tsx
+++ src/tests/BudgetTracker.test.tsx
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor, act} from '@testing-library/react';
 import App from '../App';
 import { AppProvider } from '../context/AppContext';
 
 describe('Budget Tracking Application',()=>{
-  test('creates a new expense', () =>{
-    render(<AppProvider><App/></AppProvider>)
+  test('creates a new expense', async () =>{
+    render(<AppProvider><App/></AppProvider>);
     const nameInput = screen.getByLabelText(/name/i);
     const costInput = screen.getByLabelText(/cost/i);
     const saveButton = screen.getByText(/save/i);
+
     fireEvent.change(nameInput, {target: {value: 'Test Expense'}});
     fireEvent.change(costInput, {target: {value:'50'}});
     fireEvent.click(saveButton);
-    expect(screen.getByText(/Test Expense/i)).toBeInTheDocument();
-    expect(screen.getByText(/\$50/)).toBeInTheDocument();
+
+    const newExpenseTitle = screen.getByText("Test Expense");
+    const newBudgetNum = screen.getByText("$50");
+    expect(newExpenseTitle).toBeInTheDocument;
+    expect(newBudgetNum).toBeInTheDocument;
+
+    //checking to see if the total and remaining budget are correctly updated
+    //budget should now be $950
+    await waitFor(()=> {
+        const remainingBudget = screen.getByText("$950");
+        expect(remainingBudget).toBeInTheDocument();
+    });
   });
+
+
+
+
 });
