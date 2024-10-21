eaa4ad566267473763a5b33ed0742318412b2355
kishenrex <kishenthevan@gmail.com>
2024-10-21 14:58:39 -0700

    finished exercise 3

--- src/components/Budget/Budget.tsx
+++ src/components/Budget/Budget.tsx
@@ -1,7 +1,15 @@
+import { AppContext } from "../../context/AppContext";
+import { useContext } from "react";
+
+
+
 const Budget = () => {
+
+  const { budget } = useContext(AppContext);
+
   return (
     <div className="alert alert-secondary p-3 d-flex align-items-center justify-content-between">
-      <div>Budget: $1000</div>
+      <div>Budget: ${budget}</div>
     </div>
   );
 };
