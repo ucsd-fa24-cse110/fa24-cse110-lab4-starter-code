d400c34c585316d17e94067e2b3cb9561ccd35f8
dhaDang <130107069+dhaDang@users.noreply.github.com>
2024-10-21 15:56:31 -0700

    update tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
@@ -6,4 +7,3 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
-
