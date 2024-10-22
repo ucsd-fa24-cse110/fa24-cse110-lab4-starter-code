da5dda0af89549f2ad09f987cdcc016e46b12562
Vishnu Babu Guturu <vguturu@ucsd.edu>
2024-10-21 19:28:47 -0700

    Exercise 2 done

--- src/components/Remaining.tsx
+++ src/components/Remaining.tsx
@@ -12,10 +12,14 @@ const Remaining = () => {
   const alertType = totalExpenses > budget ? "alert-danger" : "alert-success";
 
   // Exercise: Create an alert when Remaining is less than 0.
+  const remaining = budget - totalExpenses
+  if (remaining < 0){
+    window.alert("You have exceeded your budget!")
+  }
 
   return (
     <div className={`alert ${alertType}`}>
-      <span>Remaining: ${budget - totalExpenses}</span>
+      <span>Remaining: ${remaining}</span>
     </div>
   );
 };
