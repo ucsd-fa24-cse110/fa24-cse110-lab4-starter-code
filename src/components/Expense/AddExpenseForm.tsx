b389e6248afbe31135dc2d321dc22b97f8e18ed0
JonathanXiang <110417489+JonathanXiang@users.noreply.github.com>
2024-10-23 04:58:38 -0700

    Exercise 1 Done

--- src/components/Expense/AddExpenseForm.tsx
+++ src/components/Expense/AddExpenseForm.tsx
@@ -1,13 +1,27 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { AppContext } from "../../context/AppContext";
+import { Expense } from "../../types/types";
+
 const AddExpenseForm = () => {
   // Exercise: Consume the AppContext here
-
+  const {expenses, setExpenses} = useContext(AppContext);
   // Exercise: Create name and cost to state variables
+  const [name, setName] = useState("");
+  const [cost, setCost] = useState(0);
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     // Exercise: Add add new expense to expenses context array
+    const newExpense: Expense = {
+        id: name,
+        name: name,
+        cost: cost,
+    };
+    setExpenses([...expenses, newExpense]);
+
+    setName("");
+    setCost(0);
   };
 
   return (
@@ -20,8 +34,8 @@ const AddExpenseForm = () => {
             type="text"
             className="form-control"
             id="name"
-            value={""}
-            // HINT: onChange={}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           ></input>
         </div>
         <div className="col-sm">
@@ -31,8 +45,8 @@ const AddExpenseForm = () => {
             type="text"
             className="form-control"
             id="cost"
-            value={0}
-            // HINT: onChange={}
+            value={cost}
+            onChange={(e) => setCost(Number(e.target.value))}
           ></input>
         </div>
         <div className="col-sm">
--- src/components/Expense/ExpenseItem.tsx
+++ src/components/Expense/ExpenseItem.tsx
@@ -1,10 +1,16 @@
+import { useContext } from "react";
 import { Expense } from "../../types/types";
+import { AppContext } from "../../context/AppContext";
 
 const ExpenseItem = (currentExpense: Expense) => {
   // Exercise: Consume the AppContext here
-
+  const {expenses, setExpenses} = useContext(AppContext);
   const handleDeleteExpense = (currentExpense: Expense) => {
     // Exercise: Remove expense from expenses context array
+    const updatedExpenses = expenses.filter(
+        (e) => e.id !== currentExpense.id
+    );
+    setExpenses(updatedExpenses);
   };
 
   return (
