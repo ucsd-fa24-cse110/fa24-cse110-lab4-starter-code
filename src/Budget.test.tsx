62ca4b365863d4b7ea84a5c9c49cb4de99e7aecf
kishenrex <kishenthevan@gmail.com>
2024-10-23 15:53:49 -0700

    finished all budget tests

--- src/Budget.test.tsx
+++ src/Budget.test.tsx
@@ -20,16 +20,83 @@ describe("Budget Balance Verification", () => {
       expect(initialRemainingVal).toEqual("$5000");
       expect(initialSpentVal).toEqual("$0");
     });
-    test("checks ", () => {
+
+
+    test("checks budget equation is correct after adding 1 expense of $1000", () => {
       render(<App />);
-   
-      //const initialBudget = screen.getByLabelText("Budget: $")
-      //use innerHTML or assign id to the div in Budget
-      const initialBudget = screen.getByTestId('budget-value');
-      const initialBudgetVal = initialBudget.textContent;
-      //const initialBudget = screen.getByText(/Budget: \$/)
-      
-      //const initialBudgetValue = initialBudget.textContent.match(/The price is: \$(\d+)/)[1];
-      expect(initialBudgetVal).toEqual("$5000");
+      const createExpenseNameInput = screen.getByLabelText("Name");
+      const createExpenseCostInput = screen.getByLabelText("Cost");
+      const saveButton = screen.getByText("Save");
+
+      fireEvent.change(createExpenseNameInput, { target: { value: "Phone" } });
+      fireEvent.change(createExpenseCostInput, { target: { value: 1000 } });
+      fireEvent.click(saveButton);
+
+      const afterBudget = screen.getByTestId('budget-value');
+      const afterBudgetVal = afterBudget.textContent;
+
+      const afterRemaining = screen.getByTestId('remaining-value');
+      const afterRemainingVal = afterRemaining.textContent;
+
+      const afterSpent = screen.getByTestId('spent-value');
+      const afterSpentVal = afterSpent.textContent;
+
+      expect(afterBudgetVal).toEqual("$5000");
+      expect(afterRemainingVal).toEqual("$4000");
+      expect(afterSpentVal).toEqual("$1000");
+
+    });
+
+    test("adds expense then removes it", () => {
+      render(<App />);
+      const createExpenseNameInput = screen.getByLabelText("Name");
+      const createExpenseCostInput = screen.getByLabelText("Cost");
+      const saveButton = screen.getByText("Save");
+
+      fireEvent.change(createExpenseNameInput, { target: { value: "Phone" } });
+      fireEvent.change(createExpenseCostInput, { target: { value: 1000 } });
+      fireEvent.click(saveButton);
+
+      const deleteButton = screen.getByText("x");
+      fireEvent.click(deleteButton);
+
+      const afterBudget = screen.getByTestId('budget-value');
+      const afterBudgetVal = afterBudget.textContent;
+
+      const afterRemaining = screen.getByTestId('remaining-value');
+      const afterRemainingVal = afterRemaining.textContent;
+
+      const afterSpent = screen.getByTestId('spent-value');
+      const afterSpentVal = afterSpent.textContent;
+
+      expect(afterBudgetVal).toEqual("$5000");
+      expect(afterRemainingVal).toEqual("$5000");
+      expect(afterSpentVal).toEqual("$0");
+
+    });
+
+    test("adds expense that goes over budget", () => {
+      render(<App />);
+      const createExpenseNameInput = screen.getByLabelText("Name");
+      const createExpenseCostInput = screen.getByLabelText("Cost");
+      const saveButton = screen.getByText("Save");
+
+      fireEvent.change(createExpenseNameInput, { target: { value: "Phone" } });
+      fireEvent.change(createExpenseCostInput, { target: { value: 6000 } });
+      fireEvent.click(saveButton);
+
+      const afterBudget = screen.getByTestId('budget-value');
+      const afterBudgetVal = afterBudget.textContent;
+
+      const afterRemaining = screen.getByTestId('remaining-value');
+      const afterRemainingVal = afterRemaining.textContent;
+
+      const afterSpent = screen.getByTestId('spent-value');
+      const afterSpentVal = afterSpent.textContent;
+
+      expect(afterBudgetVal).toEqual("$5000");
+      expect(afterRemainingVal).toEqual("$-1000");
+      expect(afterSpentVal).toEqual("$6000");
+
     });
    });
