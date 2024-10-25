1819352ce2f6252ae6bf4dedcd32c442284293be
Yuliana Chavez <ychavez@Yulianas-MacBook-Pro.local>
2024-10-25 07:13:48 -0700

    updated app.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,18 +1,9 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
-
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders My Budget Planner title', () => {
   render(<App />);
-  const titleElement = screen.getByText(/My Budget Planner/i); // Replace with actual text in your App
+  const titleElement = screen.getByText(/My Budget Planner/i);
   expect(titleElement).toBeInTheDocument();
 });
