60a11feec7de1abcbe8c4fa82c7c4623f8112b02
Kent Giang <kentgi154@gmail.com>
2024-10-24 23:37:08 -0700

    Exercise 3 and optional challenge Done

--- src/components/Budget/Budget.tsx
+++ src/components/Budget/Budget.tsx
@@ -1,7 +1,33 @@
+import { useContext, useState } from "react";
+import { AppContext } from "../../context/AppContext";
+
 const Budget = () => {
+  const { budget, setBudget } = useContext(AppContext);
+  const [editingBudget, setEditingBudget] = useState(false);
+  const [newBudget, setNewBudget] = useState(budget);
+
+  const handleSaveBudget = () => {
+    setBudget(newBudget);
+    setEditingBudget(false);
+  };
+
   return (
-    <div className="alert alert-secondary p-3 d-flex align-items-center justify-content-between">
-      <div>Budget: $1000</div>
+    <div className="alert alert-secondary p-2 d-flex align-items-center justify-content-between">
+      {editingBudget ? (
+        <div className="d-flex align-items-center">
+          <input type="number" value={newBudget} onChange={(e) => setNewBudget(Number(e.target.value))} />
+          <button onClick={handleSaveBudget} className="btn btn-primary ms-3">
+            Save
+          </button>
+        </div>
+      ) : (
+        <div className="d-flex align-items-center">
+          <span>Budget: ${budget}</span>
+          <button onClick={() => setEditingBudget(true)} className="btn btn-secondary ms-3">
+            Edit
+          </button>
+        </div>
+      )}
     </div>
   );
 };
