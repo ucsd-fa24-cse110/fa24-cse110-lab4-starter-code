2c1c66c411122b5e5ab9c997a1da0e7021f096eb
maynhile13105 <maynhile13105@gmail.com>
2024-10-24 22:03:25 -0700

    Exercise 2 done

--- src/components/Remaining.tsx
+++ src/components/Remaining.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AppContext } from "../context/AppContext";
 
 const Remaining = () => {
@@ -10,12 +10,19 @@ const Remaining = () => {
   }, 0);
 
   const alertType = totalExpenses > budget ? "alert-danger" : "alert-success";
-
   // Exercise: Create an alert when Remaining is less than 0.
+  const remaining = budget - totalExpenses;
+
+  useEffect(()=> {
+    if(remaining<0){
+      alert("You have exceeded your budget!");
+    };
+  }, [remaining]);
+  
 
   return (
     <div className={`alert ${alertType}`}>
-      <span>Remaining: ${budget - totalExpenses}</span>
+      <span>Remaining: ${remaining}</span>
     </div>
   );
 };
