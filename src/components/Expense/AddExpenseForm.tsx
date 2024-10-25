b24d20dff2b892b47cbfd25233c765a45015a8b9
qisun724 <81499808+qisun724@users.noreply.github.com>
2024-10-25 00:02:04 -0700

    Exercise 1 done

--- src/components/Expense/AddExpenseForm.tsx
+++ src/components/Expense/AddExpenseForm.tsx
@@ -1,13 +1,28 @@
 import React, { useState } from "react";
+import { useContext } from "react";
+import { AppContext } from "../../context/AppContext";
+import { Expense } from "../../types/types";
+
 const AddExpenseForm = () => {
   // Exercise: Consume the AppContext here
+  const { expenses, setExpenses } = useContext(AppContext);
 
   // Exercise: Create name and cost to state variables
+  const [name, setName] = useState("");
+  const [cost, setCost] = useState("");
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     // Exercise: Add add new expense to expenses context array
+    const newExpense = {
+      id: String(expenses.length + 1),
+      name: name,
+      cost: Number(cost),
+    };
+    setExpenses([...expenses, newExpense]);
+    setName("");
+    setCost("");
   };
 
   return (
@@ -20,8 +35,11 @@ const AddExpenseForm = () => {
             type="text"
             className="form-control"
             id="name"
-            value={""}
+            value={name}
             // HINT: onChange={}
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
           ></input>
         </div>
         <div className="col-sm">
@@ -31,8 +49,12 @@ const AddExpenseForm = () => {
             type="text"
             className="form-control"
             id="cost"
-            value={0}
+            value={cost}
             // HINT: onChange={}
+            onChange={(event) => {
+              setCost(event.target.value);
+            }}
+            // Expenses, Remaining, and Spent so Far turns to NaN when input for Cost is non-number
           ></input>
         </div>
         <div className="col-sm">
--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -3,6 +3,8 @@ import { Expense } from "../types/types";
 
 // Exercise: Create add budget to the context
 
+
+//Define the shape of our context
 interface AppContextType {
   expenses: Expense[];
   setExpenses: React.Dispatch<React.SetStateAction<Expense[]>>;
@@ -13,10 +15,14 @@ const initialState: AppContextType = {
   setExpenses: () => {},
 };
 
+//Create the context
 export const AppContext = createContext<AppContextType>(initialState);
 
+//Create the provider component
 export const AppProvider = (props: any) => {
   const [expenses, setExpenses] = useState<Expense[]>(initialState.expenses);
+
+  //method for setExpenses?
 
   return (
     <AppContext.Provider
