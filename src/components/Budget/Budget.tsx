bd294b71e4284a1ae7c880e47f954f7e3cce0308
Sara Bailouni <sbailouni@ucsd.edu>
2024-10-25 01:28:09 -0700

    Exercise 3 Done

--- src/components/Budget/Budget.tsx
+++ src/components/Budget/Budget.tsx
@@ -1,7 +1,12 @@
+import { useContext, useEffect } from "react";
+import { AppContext } from "/Users/Sara/Desktop/fa24-cse110-lab4-starter-code/src/context/AppContext";
+
 const Budget = () => {
+  const { budget } = useContext(AppContext);
+
   return (
     <div className="alert alert-secondary p-3 d-flex align-items-center justify-content-between">
-      <div>Budget: $1000</div>
+      <div>Budget: {budget}</div>
     </div>
   );
 };
