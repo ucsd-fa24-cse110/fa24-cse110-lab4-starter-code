56504bf47615d8e1396f70439caef9afa5a3357b
Michael He <michaelhe2016@gmail.com>
2024-10-25 21:37:34 -0700

    Exercise 4 done

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders MyBudgetTracker component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/My Budget Planner/i);
   expect(linkElement).toBeInTheDocument();
 });
+
