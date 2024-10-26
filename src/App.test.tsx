e29ddf1d280f7b64ed57460573f72055128b9300
Natasha <natashamarietran@gmail.com>
2024-10-25 20:52:28 -0700

    Adding edge case where alert is triggered when budget is less than 0

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -190,6 +190,52 @@ describe("Budget Balance", () => {
     expect(remaining).toBeInTheDocument();
     expect(budget).toBeInTheDocument();
   });
+
+
+});
+
+//EDGE CASES BELOW
+describe("Edge Cases", () => {
+  test("Check that alert is triggered when over 1000 ", () => {
+    render(<App />);
+
+    //Going to make two expenses, $1000 and $1, that way we can see that expenses that total up to higher than $1000, 
+
+    //Set up
+    const createNoteNameInput = screen.getByLabelText("Name");
+    const createNoteCostInput = screen.getByLabelText("Cost");
+    const createNoteButton = screen.getByTestId("Save");
+    const alertSpy = jest.spyOn(window, 'alert').mockImplementation(() => {});
+
+
+    //First expense, $1000
+    fireEvent.change(createNoteNameInput, { target: {value: "Expense 1" } });
+    fireEvent.change(createNoteCostInput, { target: {value: 1000 } });
+    fireEvent.click(createNoteButton);
+
+    //Verifies that the expenses show up correctly
+    const noteTitleEX1 = screen.getByText("Expense 1");
+    const noteCost1K = screen.getByText("$1000");
+    expect(noteTitleEX1).toBeInTheDocument();
+    expect(noteCost1K).toBeInTheDocument();
+
+    // Second expense, $1
+    fireEvent.change(createNoteNameInput, { target: {value: "Expense 2" } });
+    fireEvent.change(createNoteCostInput, { target: {value: 1 } });
+    fireEvent.click(createNoteButton);
+
+    //Verifies that the 2nd expenses show up correctly
+    const noteTitleEX2 = screen.getByText("Expense 2");
+    const noteCost1dollar = screen.getByText("$1");
+    expect(noteTitleEX2).toBeInTheDocument();
+    expect(noteCost1dollar).toBeInTheDocument();
+
+    // Checks that the alert shows up, because this is the alerts text, got this from stack overflow
+    expect(alertSpy).toHaveBeenCalledWith("Warning: Budget is less than 0!");
+
+    //Restores the alert
+    //alertSpy.mockRestore(); NOT NEEDED
+  });
 });
 
 
