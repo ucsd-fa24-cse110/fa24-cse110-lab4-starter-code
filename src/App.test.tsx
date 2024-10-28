9d0f0e2ad4bc6296c889d1bf5cb4ded657aebcf2
chrissngn <123513732+chrissngn@users.noreply.github.com>
2024-10-28 13:32:01 -0700

    testing branch: should be wrong

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,7 +7,7 @@ import { render, screen, act } from '@testing-library/react';
 
 test('renders learn react link', () => {
   render(<AppProvider><App/></AppProvider>);
-  const linkElement = screen.getByText(/My Budget Planner/i);
-  //expect(linkElement).toBeInTheDocument();
-  expect("something").toBeInTheDocument();
+  const linkElement = screen.getByText(/My Broken LInk/i);
+  expect(linkElement).toBeInTheDocument();
+  //expect("something").toBeInTheDocument();
 });
