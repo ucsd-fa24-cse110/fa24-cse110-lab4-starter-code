8c07044e8a5d36e3dc64e510e2b064bebda4ce11
aishahmed <88351337+aishahmed@users.noreply.github.com>
2024-10-28 15:47:32 -0700

    fixed test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,6 +5,5 @@ import App from './App';
 test('renders My Budget Planner title', () => {
   render(<App />);
   const titleElement = screen.getByText(/My Budget Planner/i);
-  const expenseID = screen.getByText(/App Provider/);
-  expect(expenseID).toBeInTheDocument();
+  expect(titleElement).toBeInTheDocument();
 });
