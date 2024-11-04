7d3c7eeaaf8fefe3c8850b40a6e0c1a48e4fa9f6
maynhile13105 <maynhile13105@gmail.com>
2024-11-03 20:20:19 -0800

    setup done

---
+++ client/src/components/Expense/ExpenseItem.tsx
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react';
+import { Expense, } from "../../types/types";
+import { AppContext } from "../../context/AppContext";
+
+const ExpenseItem = (currentExpense: Expense) => {
+  // Exercise: Consume the AppContext here
+  const {expenses, setExpenses} = useContext(AppContext);
+
+  const handleDeleteExpense = (currentExpense: Expense) => {
+    // Exercise: Remove expense from expenses context array
+    setExpenses((prev)=> prev.filter((expenseItem)=> expenseItem.id !== currentExpense.id));
+  };
+
+  return (
+    <li className="list-group-item d-flex justify-content-between align-items-center">
+      <div>{currentExpense.description}</div>
+      <div>${currentExpense.cost}</div>
+      <div>
+        <button onClick={() => handleDeleteExpense(currentExpense)}>x</button>
+      </div>
+    </li>
+  );
+};
+
+export default ExpenseItem;
