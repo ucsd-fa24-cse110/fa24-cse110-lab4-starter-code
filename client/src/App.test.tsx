97349bf7357f3d767d1c55f7efc6b3abf58bfdfa
maynhile13105 <maynhile13105@gmail.com>
2024-11-03 22:49:41 -0800

    Testing: Should be correct

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -178,7 +178,7 @@ describe("Budget Balance Verification", () => {
     const spentDollarMatch = spentSlot?.match(/\$(\d+)/);
     const spentDollar = spentDollarMatch ? parseFloat(spentDollarMatch[1]) : 0;
     const total = spentDollar + remainingDollar;
-    expect(spentDollar).toBe(budget);//total
+    expect(total).toBe(budget);//total
   });
 
   test("Verify budget after adding 2 expenses and deleting 1 of them", () => {
