2ae700624d328a778ad5446a402cfd9a80002b43
maynhile13105 <maynhile13105@gmail.com>
2024-11-04 00:14:13 -0800

    back to lab 4

--- client/src/types/types.ts
+++
@@ -1,5 +0,0 @@
-export type Expense = {
-    id: string;
-    description: string;
-    cost: number;
-};
--- server/src/types.ts
+++
@@ -1,5 +0,0 @@
-export interface Expense {
-    id: string;
-    description: string;
-    cost: number;
-  }
