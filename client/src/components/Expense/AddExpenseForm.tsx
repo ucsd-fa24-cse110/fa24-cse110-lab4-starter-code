82ead9f78923e4f1823db3bfd4d07a8304f77806
root <root@Ali2048>
2024-11-08 12:29:31 -0800

    fixed the index and database

--- client/src/components/Expense/AddExpenseForm.tsx
+++ client/src/components/Expense/AddExpenseForm.tsx
@@ -16,7 +16,13 @@ const AddExpenseForm = () => {
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     // Exercise: Add add new expense to expenses context array
-    let new_expense:Expense = {id:app.expenses.length.toString(), name:name, cost:cost}; 
+    // console.log(Number((app.expenses[(app.expenses.length)-1]).id));
+    //String(Number(app.expenses[app.expenses.length-1].id)+1)
+    if (app.expenses.length > 0){      
+      var new_expense:Expense = {id:String(Number((app.expenses[(app.expenses.length)-1]).id)+1), name:name, cost:cost}; 
+    }else {
+      var new_expense:Expense = {id:String(0), name:name, cost:cost}; 
+    };
     createExpense(new_expense); //maybe like this?
     app.setExpenses([...app.expenses,new_expense]);
 
